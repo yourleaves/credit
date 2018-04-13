32b640e1e5e9c8ae847d170cd520072fa75e1ecb
mayongzhou <917123258@qq.com>
2018-04-13 18:01:48 +0800

    add api env bug

--- app/controller/api.js
+++ app/controller/api.js
@@ -4,6 +4,9 @@ const Controller = require('egg').Controller;
 
 class ApiController extends Controller {
   async index() {
+    let count = ctx.cookies.get('count');
+    count = count ? Number(count) : 0;
+    this.ctx.cookies.set('count', ++count);
     this.ctx.body = this.ctx.request.body;
   }
   async login() {
