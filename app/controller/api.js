4f034764e0ab48007faa183b76298cc2c10594a8
mayongzhou <917123258@qq.com>
2018-04-13 18:09:51 +0800

    add api env bug

--- app/controller/api.js
+++ app/controller/api.js
@@ -4,10 +4,8 @@ const Controller = require('egg').Controller;
 
 class ApiController extends Controller {
   async index() {
-    let count = ctx.cookies.get('count');
-    count = count ? Number(count) : 0;
-    this.ctx.cookies.set('count', ++count);
     this.ctx.body = this.ctx.request.body;
+    this.ctx.session.user = "123";
   }
   async login() {
     this.ctx.body = 'login';
--- app/controller/home.js
+++ app/controller/home.js
@@ -18,6 +18,7 @@ class HomeController extends Controller {
       dataType: 'json',
     });
     ctx.body = result.data;
+
   }
 }
 
