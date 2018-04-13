50d7c4e58e6c306f2ac60b80322a7ea70799d8ed
mayongzhou <917123258@qq.com>
2018-04-13 18:34:03 +0800

    add api env bug

--- app/router.js
+++ app/router.js
@@ -6,5 +6,5 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
-  router.post('/api', controller.api.index);
+  router.get('/api', controller.api.index);
 };
