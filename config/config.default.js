cd32018e00977d5695a8c451d1dd8b4c9196d733
mayongzhou <917123258@qq.com>
2018-04-16 23:16:08 +0800

    fix post

--- config/config.default.js
+++ config/config.default.js
@@ -8,7 +8,7 @@ module.exports = appInfo => {
             host: '127.0.0.1',
             port: '3306',
             user: 'root',
-            password: '',
+            password: 'youyoudai0123',
             database: 'credit',    
         },
         app: true,
