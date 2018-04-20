170ea4a6e1a8ea640475290df8bf673aa24b995f
mayongzhou <917123258@qq.com>
2018-04-20 17:08:54 +0800

    download

---
+++ app/controller/app.js
@@ -0,0 +1,69 @@
+'use strict';
+const path = require('path');
+const sendToWormhole = require('stream-wormhole');
+const Controller = require('egg').Controller;
+const fs = require('fs');
+
+class AppController extends Controller {
+  async index() {
+    const ctx = this.ctx;
+    const data = { name: 'egg' };
+    await ctx.render('app.nj');
+  }
+
+  async download() {
+
+    const filePath = path.resolve(this.app.config.static.dir, 'youyoudai.apk');
+    this.ctx.attachment('youyoudai.apk');
+    this.ctx.set('Content-Type', 'application/octet-stream');
+    this.ctx.body = fs.createReadStream(filePath);
+  }
+
+  async kefuQRImage() {
+
+    const filePath = path.resolve(this.app.config.static.dir, 'kefu.jpeg');
+    this.ctx.attachment('kefu.jpeg');
+    this.ctx.set('Content-Type', 'application/octet-stream');
+    this.ctx.body = fs.createReadStream(filePath);
+  }
+
+   async upload() {
+    const ctx = this.ctx;
+    const parts = ctx.multipart();
+    let part;
+    while ((part = await parts()) != null) {
+      if (part.length) {
+        // arrays are busboy fields
+        console.log('field: ' + part[0]);
+        console.log('value: ' + part[1]);
+        console.log('valueTruncated: ' + part[2]);
+        console.log('fieldnameTruncated: ' + part[3]);
+      } else {
+        if (!part.filename) {
+          // user click `upload` before choose a file,
+          // `part` will be file stream, but `part.filename` is empty
+          // must handler this, such as log error.
+          return;
+        }
+        // otherwise, it's a stream
+        console.log('field: ' + part.fieldname);
+        console.log('filename: ' + part.filename);
+        console.log('encoding: ' + part.encoding);
+        console.log('mime: ' + part.mime);
+        let result;
+        try {
+          result = await ctx.oss.put('card/' + part.filename, part);
+          ctx.body = result["url"];
+        } catch (err) {
+          await sendToWormhole(part);
+          throw err;
+        }
+        console.log(result);
+      }
+    }
+    console.log('and we are done parsing the form!');
+  }
+  
+}
+
+module.exports = AppController;
--- app/router.js
+++ app/router.js
@@ -5,7 +5,7 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.home.index);
+  router.get('/app', controller.app.index);
   router.get('/download', controller.home.download);
   router.get('/kefu', controller.home.kefuQRImage);
   router.post('/api', controller.api.index);
